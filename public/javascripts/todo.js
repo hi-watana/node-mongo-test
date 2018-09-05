e3a63d9bf1f2f939c3e9ed75270c7e6493d2bbee
hi-watana <hero95alive@gmail.com>
2018-09-05 13:18:18 +0900

    Enabled to send and receive POST request.

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -5,24 +5,51 @@ main_divs[0].getElementsByTagName('form')[0].getElementsByTagName('button')[0].a
     if (inputValue === '') {
         alert('You can\'t add an empty item.');
     } else {
-        var new_tr = document.createElement('tr');
-        var new_td1 = document.createElement('td');
-        var new_div = document.createElement('div');
-        new_div.classList.add('checkbox');
-        var new_label = document.createElement('input');
-        new_label.setAttribute('type', 'checkbox');
-        new_label.setAttribute('value', '');
-        new_div.appendChild(new_label);
-        new_td1.appendChild(new_div);
-        new_tr.appendChild(new_td1);
-        var new_td2 = document.createElement('td');
-        new_td2.appendChild(document.createTextNode(inputValue));
-        new_tr.appendChild(new_td2);
-        main_divs[1].getElementsByTagName('table')[0].getElementsByTagName('tbody')[0].appendChild(new_tr);
+        var url = '/'; // リクエスト先URL
+        //var data = JSON.stringify({isdone: false, content: inputValue}); // 送信データ ('param=value&...')
+        var data = {isdone: false, content: inputValue};
+        console.log(data);
+        var request = new XMLHttpRequest();
+        request.open('POST', url);
+        request.setRequestHeader('Content-Type', 'text/json');
+        //request.setRequestHeader('Content-Length', data.length);
+
+        request.responseType = 'json';
+        request.send(data);
+        console.log(data);
+
+        request.onreadystatechange = () => {
+            if (request.readyState != 4) {
+                // リクエスト中
+            } else if (request.status != 200) {
+                // 失敗
+            } else {
+                // 送信成功
+                var new_tr = document.createElement('tr');
+                var new_td1 = document.createElement('td');
+                var new_div = document.createElement('div');
+                new_div.classList.add('checkbox');
+                var new_label = document.createElement('input');
+                new_label.setAttribute('type', 'checkbox');
+                new_label.setAttribute('value', '');
+                new_div.appendChild(new_label);
+                new_td1.appendChild(new_div);
+                new_tr.appendChild(new_td1);
+                var new_td2 = document.createElement('td');
+
+                var result = request.response;
+                inputValue += ' ' + result.content;
+                new_td2.appendChild(document.createTextNode(inputValue));
+                new_tr.appendChild(new_td2);
+                main_divs[1].getElementsByTagName('table')[0].getElementsByTagName('tbody')[0].appendChild(new_tr);
+            }
+        };
+
     }
     elm.value = '';
+
 });
 
 main_divs[0].getElementsByTagName('form')[0].addEventListener('submit', (event) => {
     event.preventDefault();
-}, false)
+}, false);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
+router.use(bodyParser.json());
+
+var mongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var assert = require('assert');
+const MONGODB_URI = 'mongodb://localhost:27017';
+
+mongoose.connect(MONGODB_URI);
+
+// todos schema
+var TodoItem = new Schema({
+    isdone: Boolean,
+    content: String
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Todo App' });
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Todo App' });
+});
+
+router.post('/', (req, res, next) => {
+
+    console.log(req.body);
+    var todoItem = new TodoItem(req.body);
+
+    todoItem.save((error) => {
+        if (error) throw error;
+    })
+    
+    res.end(JSON.stringify(req.body));
 });
 
 module.exports = router;
